5714b9043f222cb14b0c0129fc74d67352936e85
SaiSoundharya <110217619+SaiSoundharya@users.noreply.github.com>
2022-08-20 08:27:27 +0530

    Create valid-identifier.kt

---
+++ valid-identifier.kt
@@ -0,0 +1,24 @@
+fun isValidIdentifier(s: String): Boolean {
+    
+    fun isValidCharacter(ch: Char) =
+    	ch=='_' || ch in '0'..'9' || ch in 'a'..'z' || ch in 'A'..'Z'
+    
+    // if string is empty or first character begins with a digit
+    if(s.isEmpty() || s[0] in '0'..'1')
+    	return false;
+    
+    for(ch in s){
+        if(!isValidCharacter(ch))
+        	return false;
+    }
+    return true;
+}
+
+fun main(args: Array<String>) {
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+}
